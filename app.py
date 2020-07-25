aba65a84bd16f6ca1b0493c9ec01accca0a19275
Petr Sicho <xsicp01@gjk.cz>
2020-07-25 14:56:15 +0200

    basic page design

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import render_template
+from flask import url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def game():
+    return render_template('index.html', script_source=url_for('static', filename='game.js'), style_source=url_for('static', filename='style.css'))
 
 
 if __name__ == '__main__':
